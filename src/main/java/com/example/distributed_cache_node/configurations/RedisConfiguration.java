7511c8f84f1eeb42d14a93966eae3f90075b3028
Marco Giuliani <thebaffo@gmail.com>
2024-11-18 15:25:13 +0100

    redis configuration cleanup

--- src/main/java/com/example/distributed_cache_node/configurations/RedisConfiguration.java
+++ src/main/java/com/example/distributed_cache_node/configurations/RedisConfiguration.java
@@ -1,12 +1,35 @@
 package com.example.distributed_cache_node.configurations;
 
+import io.lettuce.core.ReadFrom;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
+  @Value("${spring.data.redis.host}")
+  private String REDIS_HOST;
+
+  @Value("${spring.data.redis.port}")
+  private String REDIS_PORT;
+
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory() {
+    LettuceClientConfiguration clientConfig =
+        LettuceClientConfiguration.builder().readFrom(ReadFrom.REPLICA_PREFERRED).build();
+
+    RedisStandaloneConfiguration serverConfig =
+        new RedisStandaloneConfiguration(REDIS_HOST, Integer.valueOf(REDIS_PORT));
+    serverConfig.setDatabase(0);
+
+    return new LettuceConnectionFactory(serverConfig, clientConfig);
+  }
+
   @Bean
   StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
     StringRedisTemplate template = new StringRedisTemplate();
